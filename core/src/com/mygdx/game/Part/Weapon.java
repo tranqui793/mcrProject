524d04da5f2312bd9990eca47b585b94bc5275cd
Guillaume Vetter <guillaume.vetter@epfl.ch>
2019-06-05 15:23:27 +0200

    Adding weapon class

---
+++ core/src/com/mygdx/game/Part/Weapon.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.Part;
+
+public class Weapon {
+    private WeaponType type;
+
+    public WeaponType getType() {
+        return type;
+    }
+
+    public void setType(WeaponType type) {
+        this.type = type;
+    }
+}
