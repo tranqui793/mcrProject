f60a910be589cad451ba72afa55cba8e675eb738
kaerdhalis <benjamin.leguillou@heig-vd.ch>
2019-06-07 12:08:37 +0200

    implemente HeadBuilder et LegBuilder

--- core/src/com/mygdx/game/Builder/LegBuilder.java
+++ core/src/com/mygdx/game/Builder/LegBuilder.java
@@ -1,9 +1,27 @@
 package com.mygdx.game.Builder;
 
 import com.mygdx.game.Part.Leg;
+import com.mygdx.game.Part.SubPart.Shape;
 
-public class LegBuilder implements PartBuilder {
-    public Leg build() {
-        return null;
+public class LegBuilder {
+
+    private Leg leg;
+
+    public Leg getLeg() {
+
+        if(leg.getShape() == null)
+            throw new NullPointerException("Shape missing");
+
+        return leg;
+    }
+
+    public void buildNewLeg(){
+
+        leg = new Leg();
+    }
+
+    public void buildShape(Shape shape){
+
+        leg.setShape(shape);
     }
 }
